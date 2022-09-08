eed0ae1b2e1319f8ccc3002538d5b4d8cee654f3
Shreyas Garimella <shreyas.g197@gmail.com>
2022-09-08 14:48:04 -0700

    add functions for basic math to script.js

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+function add(a, b){
+    let result = a + b;
+    return console.log(result);
+}
+
+function subtract(a, b){
+    let result = a - b;
+    console.log(result);
+}
+
+function multiply(a, b){
+    let result = a * b;
+    console.log(result);
+}
+
+function divide(a, b){
+    let result = a/b;
+    console.log(result);
+}
+
+add(1, 3);
+subtract(1, 3);
+multiply(1, 3);
+divide(1, 3);
